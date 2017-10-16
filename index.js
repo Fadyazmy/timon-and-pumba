1c937abe7960ab8ee7bf98dc6710a48d37cbc956
fadyazmy <fadel@uwo.ca>
2017-10-15 21:15:23 -0400

    fixing export

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function(req, res) {
         console.log("\n\nGENERIC: Welcome to chatbot\n\n")
         // interactions.sendGenericMessage(sender)
         var regex = new RegEx(text);
-        console.log("\nDOES IT EXIST? ", regex, "\n\n");
+        console.log("\nDOES IT EXIST? ", regex.test(lyrics), "\n\n");
 				interactions.sendTextMessage(sender, "it means no worries!")
         continue
       }
