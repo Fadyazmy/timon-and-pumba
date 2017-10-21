7ad60d0f1256212eb661bf26160bac0b259adfd6
fadyazmy <fadel@uwo.ca>
2017-10-21 16:24:43 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -24,33 +24,32 @@ app.use(bodyParser.urlencoded({
 
 // index
 app.get('/', function(req, res) {
-  res.send('OK I\'m Here - chatbot in testing')
+  res.send('OK I\'m Here - chatbot in testing');
 })
 
 // for facebook verification
 app.get('/webhook/', function(req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['hub.challenge'])
+    res.send(req.query['hub.challenge']);
   } else {
-    res.send('Error, wrong token')
+    res.send('Error, wrong token');
   }
 })
 
 // to post data
 app.post('/webhook/', function(req, res) {
   console.log("REQ.BODY.entry[0]: ", req.body.entry[0]);
-  let messaging_events = req.body.entry[0].messaging
+  let messaging_events = req.body.entry[0].messaging;
 
   for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i]
-    let sender = event.sender.id
+    let event = req.body.entry[0].messaging[i];
+    let sender = event.sender.id;
 
     if (event.message && event.message.text) {
-      let text = event.message.text
+      let text = event.message.text;
       if (text === 'FADY') {
-        console.log("\n\nGENERIC: Welcome to chatbot\n\n")
+        console.log("\n\nFady when can I take off this hat!\n\n");
 
-        // interactions.sendGenericMessage(sender)
 				interactions.sendTextMessage(sender, "it means no worries man! ;)")
         continue
       }
@@ -58,13 +57,8 @@ app.post('/webhook/', function(req, res) {
       // Reply with the next line of the song ;)
       interactions.sendTextMessage(sender, interactions.getlNextline(text));
     }
-    if (event.postback) {
-      let text = JSON.stringify(event.postback)
-      interactions.sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-      continue
-    }
   }
-  res.sendStatus(200)
+  res.sendStatus(200);
 })
 
 // debug / find port
