6ed2b4b800cd43c080c65196ec98651e8b5ba0ef
fadyazmy <fadel@uwo.ca>
2017-10-22 09:02:22 -0400

    added routes

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const FB_AUTH = require('./FB_auth');
 const songs = require('./songs/index');
 const commands = require('./commands');
 
+var webhookRoutes = require('./routes/webhook');
+
 const token = FB_AUTH.GetAccessToken();
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.json())
@@ -23,45 +25,48 @@ app.use(bodyParser.urlencoded({
 console.log(interactions.getlNextline("And the land iz darc"))
 console.log("TESTING commands; ", commands['fady']);
 
+
+app.use('/webhook', webhookRoutes);
+
 // index
 app.get('/', function(req, res) {
   res.send('OK I\'m Here - chatbot in testing');
 })
 
 // for facebook verification
-app.get('/webhook/', function(req, res) {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong token');
-  }
-})
+// app.get('/webhook/', function(req, res) {
+//   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+//     res.send(req.query['hub.challenge']);
+//   } else {
+//     res.send('Error, wrong token');
+//   }
+// });
 
 // to post data
-app.post('/webhook/', function(req, res) {
-  console.log("REQ.BODY.entry[0]: ", req.body.entry[0]);
-  let messaging_events = req.body.entry[0].messaging;
-
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i];
-    let sender = event.sender.id;
-
-    if (event.message && event.message.text) {
-      let text = event.message.text;
-
-      // if text matches command
-      if (commands[interactions.removePunctAndLowerCase(text)]) {
-				interactions.sendTextMessage(sender, commands[text])
-        continue
-      }
-      else {
-        // Reply with song name and next line ;)
-        interactions.sendTextMessage(sender, interactions.getlNextline(text));  
-      }
-    }
-  }
-  res.sendStatus(200);
-})
+// app.post('/webhook/', function(req, res) {
+//   console.log("REQ.BODY.entry[0]: ", req.body.entry[0]);
+//   let messaging_events = req.body.entry[0].messaging;
+//
+//   for (let i = 0; i < messaging_events.length; i++) {
+//     let event = req.body.entry[0].messaging[i];
+//     let sender = event.sender.id;
+//
+//     if (event.message && event.message.text) {
+//       let text = event.message.text;
+//
+//       // if text matches command
+//       if (commands[interactions.removePunctAndLowerCase(text)]) {
+// 				interactions.sendTextMessage(sender, commands[text])
+//         continue
+//       }
+//       else {
+//         // Reply with song name and next line ;)
+//         interactions.sendTextMessage(sender, interactions.getlNextline(text));
+//       }
+//     }
+//   }
+//   res.sendStatus(200);
+// });
 
 // debug / find port
 app.listen(app.get('port'), function() {
---
+++ routes/webhook.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+// custom
+const interactions = require('.././interactions');
+const commands = require('.././commands');
+
+
+router.get('/', function(req, res) {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong token');
+  }
+});
+
+router.post('/', function(req, res) {
+  console.log("REQ.BODY.entry[0]: ", req.body.entry[0]);
+  let messaging_events = req.body.entry[0].messaging;
+
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i];
+    let sender = event.sender.id;
+
+    if (event.message && event.message.text) {
+      let text = event.message.text;
+
+      // if text matches command
+      if (commands[interactions.removePunctAndLowerCase(text)]) {
+				interactions.sendTextMessage(sender, commands[text])
+        continue
+      }
+      else {
+        // Reply with song name and next line ;)
+        interactions.sendTextMessage(sender, interactions.getlNextline(text));
+      }
+    }
+  }
+  res.sendStatus(200);
+});
+
+module.exports = router;
