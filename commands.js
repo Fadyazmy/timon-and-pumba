358869ea02359ecc75eba6100ae0b7b25d0b9322
fadyazmy <fadel@uwo.ca>
2017-10-23 21:05:28 -0400

    typo

--- commands.js
+++ commands.js
@@ -15,7 +15,7 @@ module.exports = {
   "what songs do you know": getSongsList(songs),
   "song list": getSongsList(songs),
   "songs": getSongsList(songs),
-  "help": "List of commands: \n> Songs \n> What songs do you know? "
+  "help": "List of commands: \n> Songs \n> What songs do you know? ",
   "hi": "Hi! You can write any song line you'd like! Or type `help` to explore more features.",
   "Hello": "Hello! You can write any song line you'd like! Or type `help` to explore more features.",
   "Get Started": "Hello stranger! Pumba and I want to welcome you our fanpage! :D"
