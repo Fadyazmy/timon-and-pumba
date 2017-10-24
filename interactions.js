38696d84f9498fd73161c6031559100ab173e08c
fadyazmy <fadel@uwo.ca>
2017-10-23 22:40:53 -0400

    async npm intalled

--- interactions.js
+++ interactions.js
@@ -1,7 +1,8 @@
-const request = require('request')
-const token = process.env.FB_PAGE_ACCESS_TOKEN
+const request = require('request');
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
 const songs = require('./songs/index');
 var levenshtein = require('fast-levenshtein');
+const request = require('async');
 
 module.exports = {
   removeDuplicates: function(string){
